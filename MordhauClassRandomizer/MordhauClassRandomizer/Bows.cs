518fa4cd5c014398878fde6c3a1c007ec6cc0dc2
NoidedApe <108410620+NoidedApe@users.noreply.github.com>
2022-07-11 18:44:19 -0400

    Add files via upload
    This is the disgustingly messy source code. Tweak it as you see fit.

---
+++ MordhauClassRandomizer/MordhauClassRandomizer/Bows.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MordhauClassRandomizer
+{
+    internal class Bows : Weapon
+    {
+       public Bows(int equipCost, string name,int iD ):base(equipCost, name, iD)
+        {
+
+        }
+    }
+}
